8ebc423bf5737562b0ff348a472861f6d0636c72
HereComesPatsky <104551803+HereComesPatsky@users.noreply.github.com>
2023-04-10 19:13:17 +1000

    Menu and map select

---
+++ map_select.gd
@@ -0,0 +1,27 @@
+extends Control
+
+var selected_map = "Map1"
+	
+
+func _ready():
+	pass
+
+
+func _process(_delta):
+	pass
+
+
+func _on_map_1_pressed():
+	selected_map = "Map1"
+
+
+func _on_map_2_pressed():
+	selected_map = "Map2"
+
+
+func _on_map_3_pressed():
+	selected_map = "Map3"
+
+
+func _on_map_4_pressed():
+	selected_map = "Map4"
